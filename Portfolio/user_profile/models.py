cf3d9294f7543bdcfab862fc628340122cef3f5d
kamalrapi <muhammad.kamal@rapidev.ae>
2021-10-15 12:21:37 +0500

    user Profile is done

--- Portfolio/user_profile/models.py
+++ Portfolio/user_profile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    """
+        user profile details of the user cv
+    """
+
+    personal_details = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='image/', default='image/default.jpg')
+    content = models.PositiveIntegerField(blank=True,null=True)
+    address = models.TextField(blank=True,null=True)
