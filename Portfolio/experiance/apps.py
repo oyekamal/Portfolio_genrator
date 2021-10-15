36dbac7f88cf2204a5190448de30df50715a67da
kamalrapi <muhammad.kamal@rapidev.ae>
2021-10-15 11:35:08 +0500

    all apps are created

---
+++ Portfolio/experiance/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExperianceConfig(AppConfig):
+    name = 'experiance'
---
+++ Portfolio/user_project/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserProjectConfig(AppConfig):
+    name = 'user_project'
