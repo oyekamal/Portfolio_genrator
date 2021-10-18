62e8d384c5947650f245fd6e8707c357fef47a8a
kamalrapi <muhammad.kamal@rapidev.ae>
2021-10-18 15:43:29 +0500

    models are ready and imported into the views

--- Portfolio/experiance/models.py
+++ Portfolio/experiance/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from skill.models import UserSkills
+from user_profile.models import User
 # Create your models here.
 
 class UserExperiance(models.Model):
@@ -8,7 +9,11 @@ class UserExperiance(models.Model):
     """
     job_title = models.CharField(max_length=255)
     description = models.TextField()
-    started_date = models.DateField()
-    end_date = models.DateField()
+    started_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # skills = models.ForeignKey(UserSkills , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.job_title
--- Portfolio/user_profile/views.py
+++ Portfolio/user_profile/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 
 from .models import User
+from education.models import UserEducation
+from experiance.models import UserExperiance
+from user_project.models import UserProject
 
 def user_details(request):
     user = User.objects.get(username="kamal")
+    education = UserEducation.objects.filter(user=user)
+    experiance = UserExperiance.objects.filter(user=user)
+    project = UserProject.objects.filter(user=user)
+
+
+    print(education)
+    print(experiance)
+    print(project)
     return render(request, 'cv/index.html',{'user': user})
 
 
