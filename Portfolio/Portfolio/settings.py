e8a75780c5ec53e9c0d546f0bb751764e8b3f6df
kamalrapi <muhammad.kamal@rapidev.ae>
2021-10-18 17:18:23 +0500

    image is getting displayed

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -137,3 +137,6 @@ STATICFILES_DIRS =[
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+print("MEDIA_ROOT----->",MEDIA_ROOT)
+print(BASE_DIR)
--- Portfolio/user_profile/views.py
+++ Portfolio/user_profile/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
-
+from django.conf import settings
 from .models import User
 from education.models import UserEducation
 from experiance.models import UserExperiance
 from user_project.models import UserProject
 
 def user_details(request):
-    user = User.objects.get(username="raheel")
+    user = User.objects.get(username="kamal")
+    # user.img = settings.MEDIA_ROOT + str(user.image)
     education = UserEducation.objects.filter(user=user)
     experiance = UserExperiance.objects.filter(user=user)
     project = UserProject.objects.filter(user=user)
 
-
+    print("settings.MEDIA_ROOT----",user.image.url)
     # print(education)
     # print(experiance)
     # print(project)
