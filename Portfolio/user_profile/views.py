f84e1420133063e6f67c188d839fd9060e656e94
raheeljadoon56 <raheel.raiz@rapidev.ae>
2021-10-18 15:07:40 +0500

    changing username to raheel in view

--- Portfolio/user_profile/views.py
+++ Portfolio/user_profile/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import User
 
 def user_details(request):
-    user = User.objects.get(username="kamal")
+    user = User.objects.get(username="raheel")
     return render(request, 'cv/index.html',{'user': user})
 
 
