4e63c3b7af40b00eeed6d6df51544fc99d3ad2f9
raheeljadoon56 <raheel.raiz@rapidev.ae>
2021-10-22 11:45:55 +0500

    skill functionality added and working

--- Portfolio/skill/models.py
+++ Portfolio/skill/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+from user_profile.models import User
+
 # Create your models here.
 class UserSkills(models.Model):
     """
     models feild created for user skills
     """
     skill_name = models.CharField(max_length=30,blank=True, null=True)
-    skill_rate= models.PositiveIntegerField()
+    skill_rate= models.DecimalField(max_digits=2 , decimal_places=0) 
+    user = models.ForeignKey(User , on_delete = models.CASCADE, blank=True, null=True) 
+
+    def __str__(self):
+        return self.skill_name
 
     
